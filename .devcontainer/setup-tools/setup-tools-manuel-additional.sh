dc9e43504b4acd44d4bf090070a8504535aa9c83
mgallegoa <manuelgallego2003@yahoo.com>
2025-04-24 12:13:49 -0500

    Dev Container : Add script to install chafa to print images.

--- .devcontainer/setup-tools/setup-tools-manuel-additional.sh
+++ .devcontainer/setup-tools/setup-tools-manuel-additional.sh
@@ -3,9 +3,18 @@
 # -e: Exit script on any error. -u: Treat unset variables as error. -o: Fail
 set -euo pipefail
 
-# Additional software used for develop, run this script after start the container
+CHAFA_VERSION="chafa-1.14.5-1-x86_64-linux-gnu"
+PATH_INSTALL_OPT="/opt/manuel"
+
+# Additional software used for development, run this script after start the container
 echo "ADDITIONAL: Install the bash-completion in the container." | tee -a $HOME/setup.log
 sudo apt install bash-completion
 sudo apt update && sudo apt install command-not-found
+
+echo "ADDITIONAL: Install chafa to show images in console ANSI standard." | tee -a $HOME/setup.log
+cd $PATH_INSTALL_OPT
+curl -Lo "$CHAFA_VERSION.tar.gz" https://hpjansson.org/chafa/releases/static/$CHAFA_VERSION.tar.gz
+tar -xzf "$CHAFA_VERSION.tar.gz"
+rm -rf "$CHAFA_VERSION.tar.gz"
 
 echo "ADDITIONAL:: Finished." | tee -a $HOME/setup.log
