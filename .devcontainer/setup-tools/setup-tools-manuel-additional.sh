57786b8abb9f54525d03b4f024757aeec0078917
mgallegoa <manuelgallego2003@yahoo.com>
2025-04-23 20:55:37 -0500

    Dev Container : Add the app command-not-fount to additional tools.

--- .devcontainer/setup-tools/setup-tools-manuel-additional.sh
+++ .devcontainer/setup-tools/setup-tools-manuel-additional.sh
@@ -6,5 +6,6 @@ set -euo pipefail
 # Additional software used for develop, run this script after start the container
 echo "ADDITIONAL: Install the bash-completion in the container." | tee -a $HOME/setup.log
 sudo apt install bash-completion
+sudo apt update && sudo apt install command-not-found
 
 echo "ADDITIONAL:: Finished." | tee -a $HOME/setup.log
